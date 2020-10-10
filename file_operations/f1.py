3000f8edc6192e2b83571cdd7987b117e64c8ba6
Jiayi Lu <215756347@qq.com>
2020-10-10 09:44:44 +0800

    Create f1.py

---
+++ file_operations/f1.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+# list all files under a folder recursively
+def list_files_recur(path, format = '.BMP'):
+    file_paths = []
+    file_names = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            if format in file or format.lower() in file:
+                file_paths.append(os.path.join(r, file))
+                file_names.append(file)
+    return([file_paths, file_names])
