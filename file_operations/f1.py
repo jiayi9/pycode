11736fb64eca1b3cd523683fdeb6b288fe2e5a4b
Jiayi Lu <215756347@qq.com>
2022-02-07 13:15:12 +0800

    Update f1.py

--- file_operations/f1.py
+++ file_operations/f1.py
@@ -14,3 +14,15 @@ def list_files_recur(path, format = '.BMP'):
                 file_paths.append(os.path.join(r, file))
                 file_names.append(file)
     return([file_paths, file_names])
+
+
+# Not recursively
+def list_files(path, FORMAT = '.xlsx'):
+    file_paths = []
+    file_names = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            if (FORMAT.upper() in file or FORMAT.lower() in file) and r == path:
+                file_paths.append(os.path.join(r, file))
+                file_names.append(file)
+    return([file_paths, file_names])
